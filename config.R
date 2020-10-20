2799c4d28d63b9865e9a811cf6063480a0e266f4
Silvia_R <silvia.rnr@gmail.com>
2020-10-20 14:37:41 +0200

    Add files via upload

---
+++ config.R
@@ -0,0 +1,28 @@
+#####################################################
+#####################################################
+# This config file is used to set the file paths
+# for the files that are required to perform analyses
+# in /code_paper
+#####################################################
+#####################################################
+
+
+# Differentially abundant genes 
+kegg="https://ndownloader.figshare.com/files/25114706"
+
+# Differentially abundant species 
+species="https://figshare.com/s/4c26952c4f8dfd9d0d76"
+
+# Genes by Species
+
+# OGTT
+ogtt = "https://figshare.com/s/8cd771d4133f4c9c4a0f"
+# Weight chart
+
+# Food Intake
+
+
+# Metadata mapping sample names to metadata variables
+map="https://figshare.com/s/6800faaecb15085baa5a"
+
+
